664695f7036115533b758fea5ca2f9367d7cff54
Cruor <gt.hammerstad@gmail.com>
2019-03-03 17:21:19 +0100

    Now using the correct env variable for Linux

--- src/file_locations.lua
+++ src/file_locations.lua
@@ -1,7 +1,7 @@
 -- Temporary for now
 local function getResourceDir()
     local appdata = os.getenv("LocalAppData")
-    local home = os.getenv("home")
+    local home = os.getenv("HOME")
 
     if appdata then
         return appdata .. "/Loenn"
