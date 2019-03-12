c6638051b9190f163d8f20e1f032af345c470f7b
Vexatos <Stuarzt@gmx.de>
2019-03-12 23:03:49 +0100

    Added relatively decent loading screen I think.

---
+++ src/loading.lua
@@ -0,0 +1,40 @@
+local fonts = require("fonts")
+
+local loading = {}
+
+loading.spriteSheet = love.graphics.newImage("assets/loading-256.png")
+loading.quadSize = 256
+loading.quads = {}
+ 
+for y = 0, loading.spriteSheet:getHeight() - loading.quadSize, loading.quadSize do
+    for x = 0, loading.spriteSheet:getWidth() - loading.quadSize, loading.quadSize do
+        table.insert(loading.quads, love.graphics.newQuad(x, y, loading.quadSize, loading.quadSize, loading.spriteSheet:getDimensions()))
+    end
+end
+ 
+loading.duration = 1
+loading.currentTime = 0
+
+loading.text = "Loading"
+loading.alignment = "left"
+loading.textScale = fonts.fontScale * 2
+loading.textOffsetX = (fonts.font:getWidth(loading.text .. "..") * loading.textScale) / 2
+loading.textOffsetY = loading.quadSize / 2
+
+function loading:drawLoadScreen(viewport)
+    local currentQuad = math.floor(self.currentTime / self.duration * #self.quads) + 1
+
+    local dots = string.rep(".", currentQuad - 1)
+
+    love.graphics.printf(loading.text .. dots, viewport.width / 2 - loading.textOffsetX, viewport.height / 2 + loading.textOffsetY, viewport.width, loading.alignment, 0, loading.textScale)
+    love.graphics.draw(self.spriteSheet, self.quads[currentQuad], (viewport.width - self.quadSize) / 2, (viewport.height - self.quadSize) / 2, 0, 1)
+end
+
+function loading:update(dt)
+    self.currentTime = self.currentTime + dt
+    if self.currentTime >= self.duration then
+        self.currentTime = self.currentTime - self.duration
+    end
+end
+
+return loading
--- src/selene_main.lua
+++ src/selene_main.lua
@@ -12,6 +12,7 @@ require("input_handler")
 local celesteRender = require("celeste_render")
 local fileLocations = require("file_locations")
 local fonts = require("fonts")
+local loading = require("loading")
 local tasks = require("task")
 local entities = require("entities")
 local viewerState = require("loaded_state")
@@ -46,37 +47,16 @@ function love.draw()
         love.graphics.printf("FPS: " .. tostring(love.timer.getFPS()), 20, 40, viewport.width, "left", 0, fonts.fontScale, fonts.fontScale)
 
     else
-        local time = love.timer.getTime()
-        local offsetX = math.sin(time) * 32
-        local offsetY = math.cos(time) * 64
-
-        local x = viewport.width / 2 + offsetX
-        local y = viewport.height / 2 +  offsetY
-
-        local baseText = "Loading"
-        local reps = math.floor(time * 2) % 3 + 1
-        local dots = string.rep(".", reps)
-
-        love.graphics.push()
-
-        love.graphics.setColor(math.abs(math.sin(time)), math.abs(math.cos(time)), math.abs(math.tan(time)))
-
-        love.graphics.translate(x, y)
-        love.graphics.rotate(time * 0.3)
-        love.graphics.translate(offsetX, offsetY)
-        love.graphics.printf(baseText .. dots, 0, 0, viewport.width, "left", time, fonts.fontScale * 2, fonts.fontScale * 2)
-
-        love.graphics.setColor(1.0, 1.0, 1.0)
-
-        love.graphics.pop()
+        loading:drawLoadScreen(viewport)
     end
 end
 
-function love.update()
+function love.update(dt)
     if viewerState.map then
         tasks.processTasks(math.huge, 1)
 
     else
         tasks.processTasks(1 / 144)
+        loading:update(dt)
     end
 end
