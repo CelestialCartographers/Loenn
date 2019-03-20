f823fa02787a23b90b99047b71a3cb2240267217
Cruor <cruoratmc@gmail.com>
2019-03-20 15:28:05 +0100

    Started working on configs

---
+++ src/config.lua
@@ -0,0 +1,49 @@
+local utils = require("utils")
+
+local config = {}
+
+local configMt = {}
+configMt.__index = {}
+
+-- TODO - Make it possible to check file on disk for changes on read and write changes to disk on write
+
+function config.readConfig(filename)
+    local res
+    local fh = io.open(filename, "rb")
+
+    if fh then
+        local content = fh:read("*a")
+
+        res = utils.unserialize(content)
+    end
+
+    fh:close()
+
+    return res
+end
+
+function config.writeConfig(filename, data, pretty)
+    local pretty = pretty == nil or pretty
+    local success, content = false, nil
+
+    if data then
+        fh = io.open(filename, "wb")
+
+        success, content = utils.serialize(data, pretty)
+
+        if success then
+            if fh then
+                fh:write(content)
+                fh:close()
+
+            else
+                success = false
+            end
+        end
+
+    end
+
+    return success
+end
+
+return config
--- src/file_locations.lua
+++ src/file_locations.lua
@@ -20,4 +20,26 @@ function fileLocations.getResourceDir()
     end
 end
 
+function fileLocations.getStorageDir()
+    local userOS = love.system.getOS()
+
+    if userOS == "Windows" then
+        return os.getenv("LocalAppData") .. "/Lönn"
+
+    elseif userOS == "Linux" then
+        -- TODO - Is this good enough? Better alternative?
+        -- Doesn't work on Linux when creating folders for some reason?
+        return os.getenv("HOME") .. "/.lönn"
+
+    elseif userOS == "OS X" then
+        -- TODO
+
+    elseif userOS == "Android" then
+        -- TODO
+
+    elseif userOS == "iOS" then
+        -- TODO
+    end
+end
+
 return fileLocations
