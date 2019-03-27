3415f91d4858c84222cbfab314ab9ab43fd6d71a
Cruor <cruoratmc@gmail.com>
2019-03-27 08:59:42 +0100

    Added simple hotkey code

---
+++ src/keyboard_helper.lua
@@ -0,0 +1,20 @@
+local keyboard = {}
+
+function keyboard.modifierControl()
+    return love.keyboard.isDown("lctrl") or love.keyboard.isDown("rctrl")
+end
+
+function keyboard.modifierShift()
+    return love.keyboard.isDown("lshift") or love.keyboard.isDown("rshift")
+end
+
+function keyboard.modifierAlt()
+    return love.keyboard.isDown("lalt") or love.keyboard.isDown("ralt")
+end
+
+-- Command on Mac, Windows key on Windows
+function keyboard.modifierGUI()
+    return love.keyboard.isDown("lgui") or love.keyboard.isDown("rgui")
+end
+
+return keyboard
---
+++ src/structs/hotkey.lua
@@ -0,0 +1,74 @@
+local keyboardHelper = require("keyboard_helper")
+
+local specialActivators = {
+    ctrl = keyboardHelper.modifierControl,
+    shift = keyboardHelper.modifierShift,
+    alt = keyboardHelper.modifierAlt,
+    gui = keyboardHelper.modifierGUI,
+    command = keyboardHelper.modifierGUI,
+    winkey = keyboardHelper.modifierGUI,
+}
+
+local hotkeyStruct = {}
+
+-- TODO - Validate that the key constant exists?
+function hotkeyStruct.sanitize(activator)
+    local parts = string.split(activator, "+")
+    local activators = $()
+
+    for i, part <- parts do
+        local part = part:match("^%s*(.-)%s*$"):lower
+
+        if specialActivators[part] then
+            activators += specialActivators[part]
+
+        else
+            activators += part
+        end
+    end
+
+    return activators
+end
+
+function hotkeyStruct.hotkeyActive(hotkey)
+    for i, part <- hotkey.activator do
+        if type(part) == "function" then
+            if not part() then
+                return false
+            end
+
+        else
+            if not love.keyboard.isDown(part) then
+                return false
+            end
+        end
+    end
+
+    return true
+end
+
+function hotkeyStruct.callbackIfActive(hotkey)
+    if hotkeyStruct.hotkeyActive(hotkey) then
+        hotkey.callback()
+    end
+end
+
+local hotkeyMt = {}
+hotkeyMt.__index = {}
+
+hotkeyMt.__index.active = hotkeyStruct.hotkeyActive
+hotkeyMt.__index.callbackIfActive = hotkeyStruct.callbackIfActive
+
+function hotkeyStruct.createHotkey(activator, callback)
+    local hotkey = {
+        _type = "hotkey",
+        _rawActivator = activator
+    }
+
+    hotkey.callback = callback
+    hotkey.activator = hotkeyStruct.sanitize(activator)
+
+    return setmetatable(hotkey, hotkeyMt)
+end
+
+return hotkeyStruct
