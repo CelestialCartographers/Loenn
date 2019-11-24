fdcd89141bd2c4b7caebd5e93800fe9f2a6664f2
Maik Macho <nuda1998@gmail.com>
2019-11-25 00:17:41 +0100

    Update OlympUI, add dummy sidebar lists

--- src/scenes/editor.lua
+++ src/scenes/editor.lua
@@ -12,6 +12,10 @@ function editorScene:firstEnter()
     local inputDevice = require("input_device")
     local standardHotkeys = require("standard_hotkeys")
 
+    local filesystem = require("filesystem")
+    local loadedState = require("loaded_state")
+    local fileLocations = require("file_locations")
+
     local ui = require("ui")
     local uie = require("ui.elements")
     local uiu = require("ui.utils")
@@ -29,20 +33,20 @@ function editorScene:firstEnter()
     --]]
     uie.__label.__default.style.font = love.graphics.newFont(16)
 
-    local root = uie.group({
+    local root = uie.column({
         uie.topbar({
             { "File", {
                 { "New" },
                 { },
-                { "Open" },
+                { "Open", (-> filesystem.openDialog(fileLocations.getCelesteDir(), nil, loadedState.loadFile)) },
                 { "Recent", {
                     { "Totally" },
                     { "Not A Dummy" },
                     { "Nested Submenu" }
                 } },
                 { },
-                { "Save" },
-                { "Save As..." },
+                { "Save", (-> loadedState.filename and loadedState.saveFile(loadedState.filename)) },
+                { "Save As...", (-> loadedState.side and filesystem.saveDialog(loadedState.filename, nil, loadedState.saveFile)) },
                 { },
                 { "Settings" },
                 { },
@@ -75,49 +79,115 @@ function editorScene:firstEnter()
             }},
         }),
 
-        uie.window("Windowception",
+        uie.group({
             uie.scrollbox(
-                uie.group({
-                    uie.window("Child 1", uie.column({ uie.label("Oh no") })):with({ x = 10, y = 10}),
-                    uie.window("Child 2", uie.column({ uie.label("Oh no two") })):with({ x = 30, y = 30})
-                }):with({ width = 200, height = 400 })
-            ):with({ width = 200, height = 200 })
-        ):with({ x = 50, y = 100 }),
+                uie.list(
+                    uiu.map(uiu.listRange(200, 1, -1), function(i)
+                        return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                    end)
+                )
+            ):with({
+                calcWidth = (el -> el._inner.width)
+            }):with(uiu.fillHeight),
 
-        uie.window("Hello, World!",
             uie.column({
-                uie.label("This is a one-line label."),
-                
-                -- Labels use Löve2D Text objects under the hood.
-                uie.label({ { 1, 1, 1 }, "This is a ", { 1, 0, 0 }, "colored", { 0, 1, 1 }, " label."}),
-
-                -- Multi-line labels aren't subjected to the parent element's spacing property.
-                uie.label("This is a two-line label.\nThe following label is updated dynamically."),
-
-                -- Dynamically updated label.
-                uie.label():as("info"),
-
-                uie.button("This is a button.", function(btn)
-                    if btn.counter == nil then
-                        btn.counter = 0
+                uie.listH({
+                    { "A" },
+                    { "B" },
+                    { "C" },
+                    { "D" },
+                    { "E" }
+                }):with(uiu.hook{
+                    layoutLateLazy = function(orig, el)
+                        local parentWidth = el.parent.innerWidth
+                        if el.width < parentWidth then
+                            el.width = parentWidth
+                            el.innerWidth = parentWidth - el.style.padding * 2
+                        end
+                        orig(el)
                     end
-                    btn.counter = btn.counter + 1
-                    btn.text = "Pressed " .. tostring(btn.counter) .. " time" .. (btn.counter == 1 and "" or "s")
-                end),
+                }),
 
-                uie.button("Disabled"):with({ enabled = false }),
+                uie.field("<TODO: TEXT INPUT>"),
 
-                uie.button("Useless"),
+                uie.scrollbox(
+                    uie.list(
+                        uiu.map(uiu.listRange(200, 1, -1), function(i)
+                            return { text = string.format("%i%s", i, i % 7 == 0 and " (something)" or ""), data = i }
+                        end)
+                    ):with(uiu.hook{
+                        layoutLateLazy = function(orig, el)
+                            local parentWidth = el.parent.innerWidth
+                            if el.width < parentWidth then
+                                el.width = parentWidth
+                                el.innerWidth = parentWidth - el.style.padding * 2
+                            end
+                            orig(el)
+                        end
+                    })
+                ):with(uiu.fillHeight(true))
+                :with(uiu.hook{
+                    calcWidth = (orig, el -> el._inner.width),
+                    layoutLateLazy = function(orig, el)
+                        local parentWidth = el.parent.innerWidth
+                        if el.width < parentWidth then
+                            el.width = parentWidth
+                            el.innerWidth = parentWidth - el.style.padding * 2
+                        end
+                        orig(el)
+                    end
+                })
+            }):with(uiu.fillHeight):with(uiu.rightbound):with({
+                style = {
+                    padding = 0
+                }
+            }),
 
-                uie.label("Select an item from the list below."):as("selected"),
-                uie.list(uiu.map(uiu.listRange(1, 3), function(i)
-                    return { text = string.format("Item %i!", i), data = i }
-                end), function(list, item)
-                    list.parent._selected.text = "Selected " .. tostring(item)
-                end)
+            uie.window("Windowception",
+                uie.scrollbox(
+                    uie.group({
+                        uie.window("Child 1", uie.column({ uie.label("Oh no") })):with({ x = 10, y = 10}),
+                        uie.window("Child 2", uie.column({ uie.label("Oh no two") })):with({ x = 30, y = 30})
+                    }):with({ width = 200, height = 400 })
+                ):with({ width = 200, height = 200 })
+            ):with({ x = 50, y = 100 }),
 
-            })
-        ):with({ x = 200, y = 50 }):as("test"),
+            uie.window("Hello, World!",
+                uie.column({
+                    uie.label("This is a one-line label."),
+
+                    -- Labels use Löve2D Text objects under the hood.
+                    uie.label({ { 1, 1, 1 }, "This is a ", { 1, 0, 0 }, "colored", { 0, 1, 1 }, " label."}),
+
+                    -- Multi-line labels aren't subjected to the parent element's spacing property.
+                    uie.label("This is a two-line label.\nThe following label is updated dynamically."),
+
+                    -- Dynamically updated label.
+                    uie.label():as("info"),
+
+                    uie.button("This is a button.", function(btn)
+                        if btn.counter == nil then
+                            btn.counter = 0
+                        end
+                        btn.counter = btn.counter + 1
+                        btn.text = "Pressed " .. tostring(btn.counter) .. " time" .. (btn.counter == 1 and "" or "s")
+                    end),
+
+                    uie.button("Disabled"):with({ enabled = false }),
+
+                    uie.button("Useless"),
+
+                    uie.label("Select an item from the list below."):as("selected"),
+                    uie.list(uiu.map(uiu.listRange(1, 3), function(i)
+                        return { text = string.format("Item %i!", i), data = i }
+                    end), function(list, item)
+                        list.parent._selected.text = "Selected " .. tostring(item)
+                    end)
+
+                })
+            ):with({ x = 200, y = 50 }):as("test"),
+
+        }):with(uiu.fillWidth):with(uiu.fillHeight(true)),
     }):with({
         style = {
             bg = { bg = {} },
@@ -128,7 +198,7 @@ function editorScene:firstEnter()
         clip = false,
         cacheable = false
     })
-    
+
     ui.init(root, false)
     inputDevice.newInputDevice(self.inputDevices, ui)
 
