5bff762ab027ae0546eb178760cb33bca2902647
Vexatos <Stuarzt@gmx.de>
2019-12-14 18:04:50 +0100

    Made Lönn load on macos.

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,11 @@
 function love.load()
     local path = "selene/selene/lib/?.lua;selene/selene/lib/?/init.lua;" .. love.filesystem.getRequirePath()
-
     love.filesystem.setRequirePath(path)
 
-    love.filesystem.mount(love.filesystem.getSourceBaseDirectory(), "", true)
+    -- The English language contains over 500,000 words and not a single one of them is suitable for describing just how much I want to purge macos from existence.
+    if love.system.getOS() == "OS X" then
+        package.cpath = package.cpath .. ";" .. love.filesystem.getSourceBaseDirectory() .. "/?.so"
+    end
 
     require("selene").load()
     require("selene/selene/wrappers/searcher/love2d/searcher").load()
