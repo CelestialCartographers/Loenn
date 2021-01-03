4868526d5f494430d9db42655a8f34d5d187f8a2
Cruor <cruoratmc@gmail.com>
2021-01-03 14:42:58 +0100

    Simplified crystal heart entity Added placement to refill

--- src/entities/blackGem.lua
+++ src/entities/blackGem.lua
@@ -1,16 +1,9 @@
-local drawableSpriteStruct = require("structs.drawable_sprite")
-
 local heart = {}
 
 heart.depth = -2000000
+heart.texture = "collectables/heartGem/0/00"
 heart.placements = {
     name = "Crystal Heart"
 }
-
-local texture = "collectables/heartGem/0/00"
-
-function heart.sprite(room, entity)
-    return drawableSpriteStruct.spriteFromTexture(texture, entity)
-end
 
 return heart
--- src/entities/refill.lua
+++ src/entities/refill.lua
@@ -1,6 +1,9 @@
 local refill = {}
 
 refill.depth = -100
+refill.placements = {
+    name = "Refill"
+}
 
 function refill.texture(room, entity)
     return entity.twoDash and "objects/refillTwo/idle00" or "objects/refill/idle00"
