2b7557ea47fa7f369e0d4cba36ea03c0c76de0d9
Cruor <cruoratmc@gmail.com>
2021-01-30 21:21:49 +0100

    Made menubar a bit more functional Still has a lot of placeholders, but some of the options are functional

--- src/ui/menubar.lua
+++ src/ui/menubar.lua
@@ -2,12 +2,62 @@ local ui = require("ui")
 local uiElements = require("ui.elements")
 local uiUtils = require("ui.utils")
 
--- TODO - Implement properly
+local loadedState = require("loaded_state")
+local history = require("history")
+local debugUtils = require("debug_utils")
+
 local menubar = {}
 
 menubar.menubar = {
-    {"Test 1"},
-    {"Test 2"}
+    {"File", {
+        {"New"},
+        {"Open", loadedState.openMap},
+        {"Recent", {
+            {"Here"},
+            {"Be"},
+            {"Recents"}
+        }},
+        {},
+        {"Save", loadedState.saveCurrentMap},
+        {"Save As", loadedState.saveAsCurrentMap},
+        {},
+        {"Exit", love.event.quit}
+    }},
+    {"Edit", {
+        {"Undo", history.undo},
+        {"Redo", history.redo},
+        {},
+        {"Settings"}
+    }},
+    {"View", {
+
+    }},
+    {"Map", {
+        {"Stylegrounds"},
+        {"Metadata"}
+    }},
+    {"Room", {
+        {"Add"},
+        {"Configure"},
+        {},
+        {"Delete"}
+    }},
+    {"Debug", {
+        {"Reload", {
+            {"Scenes", debugUtils.reloadScenes},
+            {"Tools", debugUtils.reloadTools},
+            {"Entities", debugUtils.reloadEntities},
+            {"Triggers"},
+            {"Effects"},
+            {"Language Files"}
+        }},
+        {"Redraw Map", debugUtils.redrawMap},
+        {"Test Console", debugUtils.debug}
+    }},
+    {"Help", {
+        {"Check for updates"},
+        {"About"}
+    }}
 }
 
 function menubar.getMenubar()
