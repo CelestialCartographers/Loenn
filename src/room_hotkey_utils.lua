07cfc2049eb6ab82deb4d9b002eecfca06765d4a
Cruor <cruoratmc@gmail.com>
2021-02-07 17:50:43 +0100

    Hotkey room/filler actions now adds to history

--- src/room_hotkey_utils.lua
+++ src/room_hotkey_utils.lua
@@ -5,6 +5,9 @@ local movementUtils = {}
 local loadedState = require("loaded_state")
 local roomStruct = require("structs.room")
 local fillerStruct = require("structs.filler")
+local snapshotUtils = require("snapshot_utils")
+local history = require("history")
+local utils = require("utils")
 
 local directions = {
     Left = "left",
@@ -13,42 +16,62 @@ local directions = {
     Down = "down"
 }
 
-local function getItemStruct(itemType)
-    if itemType == "room" then
-        return roomStruct
+local itemStructs = {
+    room = roomStruct,
+    filler = fillerStruct
+}
 
-    elseif itemType == "filler" then
-        return fillerStruct
+local snapshotFunctions = {
+    room = snapshotUtils.roomSnapshot,
+    filler = snapshotUtils.fillerSnapshot
+}
+
+-- TODO - Proper snapshot description
+local function callWithHistory(func, itemType, item, ...)
+    local snapshotFunction = snapshotFunctions[itemType]
+
+    if item then
+        local itemBefore = utils.deepcopy(item)
+        local res = func(item, ...)
+        local itemAfter = utils.deepcopy(item)
+
+        local snapshot = snapshotFunction(item, "Room movement", itemBefore, itemAfter)
+
+        history.addSnapshot(snapshot)
+
+        return true
     end
+
+    return false
 end
 
 for name, direction in pairs(directions) do
     movementUtils["moveCurrentRoomOneTile" .. name] = function()
         local item, itemType = loadedState.getSelectedItem()
-        local itemStruct = getItemStruct(itemType)
+        local itemStruct = itemStructs[itemType]
 
-        return item and itemStruct.directionalMove(item, direction, 1)
+        return callWithHistory(itemStruct.directionalMove, itemType, item, direction, 1)
     end
 
     movementUtils["moveCurrentRoomOnePixel" .. name] = function()
         local item, itemType = loadedState.getSelectedItem()
-        local itemStruct = getItemStruct(itemType)
+        local itemStruct = itemStructs[itemType]
 
-        return item and itemStruct.directionalMove(item, direction, 1, 1)
+        return callWithHistory(itemStruct.directionalMove, itemType, item, direction, 1, 1)
     end
 
     movementUtils["growCurrentRoomOneTile" .. name] = function()
         local item, itemType = loadedState.getSelectedItem()
-        local itemStruct = getItemStruct(itemType)
+        local itemStruct = itemStructs[itemType]
 
-        return item and itemStruct.directionalResize(item, direction, 1)
+        return callWithHistory(itemStruct.directionalResize, itemType, item, direction, 1)
     end
 
     movementUtils["shrinkCurrentRoomOneTile" .. name] = function()
         local item, itemType = loadedState.getSelectedItem()
-        local itemStruct = getItemStruct(itemType)
+        local itemStruct = itemStructs[itemType]
 
-        return item and itemStruct.directionalResize(item, direction, -1)
+        return callWithHistory(itemStruct.directionalResize, itemType, item, direction, -1)
     end
 end
 
