affba198dd60c4b5186d7eaf93b89cc75318c10e
Cruor <cruoratmc@gmail.com>
2021-02-11 12:49:35 +0100

    Simplified jumpthru by using the new sub quad caching system

--- src/entities/jump_thru.lua
+++ src/entities/jump_thru.lua
@@ -9,13 +9,6 @@ local function getTexture(entity)
     return entity.texture and entity.texture ~= "default" and entity.texture or "wood"
 end
 
-local quads = {
-    {0, 0, 8, 7}, {8, 0, 8, 7}, {16, 0, 8, 7},
-    {0, 8, 8, 5}, {8, 8, 8, 5}, {16, 8, 8, 5}
-}
-
-local quadCache = {}
-
 local jumpthru = {}
 
 jumpthru.name = "jumpThru"
@@ -32,16 +25,6 @@ for i, texture in ipairs(textures) do
     }
 end
 
-local function getQuad(sprite, texture, index)
-    quadCache[texture] = quadCache[texture] or {}
-
-    if not quadCache[texture][index] then
-        quadCache[texture][index] = drawing.getRelativeQuad(sprite.meta, unpack(quads[index]))
-    end
-
-    return quadCache[texture][index]
-end
-
 function jumpthru.sprite(room, entity)
     local textureRaw = getTexture(entity)
     local texture = "objects/jumpthru/" .. textureRaw
@@ -56,24 +39,23 @@ function jumpthru.sprite(room, entity)
     local sprites = {}
 
     for i = 0, len do
-        local connected = 1
-        local qx = 2
+        local quadX = 8
+        local quadY = 8
 
         if i == 0 then
-            connected = room.tilesFg.matrix:get(startX - 1, startY, "0") ~= "0" and 0 or 1
-            qx = 1
+            quadX = 0
+            quadY = room.tilesFg.matrix:get(startX - 1, startY, "0") ~= "0" and 0 or 8
 
         elseif i == len then
-            connected = room.tilesFg.matrix:get(stopX + 1, startY, "0") ~= "0" and 0 or 1
-            qx = 3
+            quadY = room.tilesFg.matrix:get(stopX + 1, startY, "0") ~= "0" and 0 or 8
+            quadX = 16
         end
 
         local sprite = drawableSpriteStruct.spriteFromTexture(texture, entity)
-        sprite:addPosition(i * 8, 0)
-        sprite:setOffset(0, 0)
 
-        local index = connected * 3 + qx
-        sprite.quad = getQuad(sprite, texture, index)
+        sprite:setJustification(0, 0)
+        sprite:addPosition(i * 8, 0)
+        sprite:useRelativeQuad(quadX, quadY, 8, 8)
 
         table.insert(sprites, sprite)
     end
