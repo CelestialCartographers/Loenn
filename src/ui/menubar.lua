60a186cc9af6188da9066731b7b83c20f80289e6
Cruor <cruoratmc@gmail.com>
2021-02-17 15:52:03 +0100

    Menubar now uses language file Started working on language file for UI

--- src/ui/menubar.lua
+++ src/ui/menubar.lua
@@ -6,64 +6,83 @@ local loadedState = require("loaded_state")
 local history = require("history")
 local debugUtils = require("debug_utils")
 
+local utils = require("utils")
+local languageRegistry = require("language_registry")
+
 local menubar = {}
 
 menubar.menubar = {
-    {"File", {
-        {"New"},
-        {"Open", loadedState.openMap},
-        {"Recent", {
-            {"Here"},
-            {"Be"},
-            {"Recents"}
-        }},
+    {"file", {
+        {"new"},
+        {"open", loadedState.openMap},
+        {"recent", {}},
         {},
-        {"Save", loadedState.saveCurrentMap},
-        {"Save As", loadedState.saveAsCurrentMap},
+        {"save", loadedState.saveCurrentMap},
+        {"save_as", loadedState.saveAsCurrentMap},
         {},
-        {"Exit", love.event.quit}
+        {"exit", love.event.quit}
     }},
-    {"Edit", {
-        {"Undo", history.undo},
-        {"Redo", history.redo},
+    {"edit", {
+        {"undo", history.undo},
+        {"redo", history.redo},
         {},
-        {"Settings"}
+        {"settings"}
     }},
-    {"View", {
+    {"view", {
 
     }},
-    {"Map", {
-        {"Stylegrounds"},
-        {"Metadata"}
+    {"map", {
+        {"stylegrounds"},
+        {"metadata"}
     }},
-    {"Room", {
-        {"Add"},
-        {"Configure"},
+    {"room", {
+        {"add"},
+        {"configure"},
         {},
-        {"Delete"}
+        {"delete"}
     }},
-    {"Debug", {
-        {"Reload", {
-            {"Everything", debugUtils.reloadEverything},
-            {"Scenes", debugUtils.reloadScenes},
-            {"Tools", debugUtils.reloadTools},
-            {"Entities", debugUtils.reloadEntities},
-            {"Triggers", debugUtils.reloadTriggers},
-            {"Effects"},
-            {"User Interface", function() debugUtils.reloadUI() end}, -- debugUtils.reloadUI might change, call in function
-            {"Language Files", debugUtils.reloadLanguageFiles}
+    {"debug", {
+        {"reload", {
+            {"reload_everything", debugUtils.reloadEverything},
+            {"reload_scenes", debugUtils.reloadScenes},
+            {"reload_tools", debugUtils.reloadTools},
+            {"reload_entities", debugUtils.reloadEntities},
+            {"reload_triggers", debugUtils.reloadTriggers},
+            {"reload_effects"},
+            {"reload_user_interface", function() debugUtils.reloadUI() end}, -- debugUtils.reloadUI might change, call in function
+            {"reload_language_files", debugUtils.reloadLanguageFiles}
         }},
-        {"Redraw Map", debugUtils.redrawMap},
-        {"Test Console", debugUtils.debug}
+        {"redraw_map", debugUtils.redrawMap},
+        {"test_console", debugUtils.debug}
     }},
-    {"Help", {
-        {"Check for updates"},
-        {"About"}
+    {"help", {
+        {"check_for_updates"},
+        {"about"}
     }}
 }
 
+local function addLanguageStrings(menu, language)
+    if type(menu) == "table" then
+        local languageKey = menu[1]
+        local languageKeyType = type(languageKey)
+
+        if languageKeyType == "string" then
+            menu[1] = tostring(language.ui.menubar[languageKey])
+        end
+
+        for _, sub in pairs(menu) do
+            addLanguageStrings(sub, language)
+        end
+    end
+end
+
 function menubar.getMenubar()
-    return uiElements.topbar(menubar.menubar)
+    local preparedMenubar = utils.deepcopy(menubar.menubar)
+    local language = languageRegistry.getLanguage()
+
+    addLanguageStrings(preparedMenubar, language)
+
+    return uiElements.topbar(preparedMenubar)
 end
 
 return menubar
