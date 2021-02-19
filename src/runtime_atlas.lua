e97de93321df9259c6ecc13d2660a6ea0b0d57a9
Cruor <cruoratmc@gmail.com>
2021-02-19 22:35:13 +0100

    Runtime atlases now does some safety checks Adding images past canvas array count now works

--- src/runtime_atlas.lua
+++ src/runtime_atlas.lua
@@ -8,8 +8,28 @@ textureAtlas._MT = {}
 textureAtlas.width = 4096
 textureAtlas.height = 4096
 
-textureAtlas.canvases = 16
-textureAtlas.canvasArray = love.graphics.newCanvas(textureAtlas.width, textureAtlas.height, textureAtlas.canvases, {type="array"})
+textureAtlas.canvases = nil
+textureAtlas.canvasArray = nil
+
+-- Set up canvasArray
+-- Love2d on windows doesn't report how many layers are actually supported due to a bug
+-- Test a few, worst case default to 0
+function textureAtlas.init()
+    local layerCounts = {16, 8, 4}
+
+    for _, count in ipairs(layerCounts) do
+        local success, canvas = pcall(love.graphics.newCanvas, 4096, 4096, count, {type="array"})
+
+        if success then
+            textureAtlas.canvases = count
+            textureAtlas.canvasArray = canvas
+
+            return count
+        end
+    end
+
+    textureAtlas.canvases = 0
+end
 
 function textureAtlas.clear()
     for i, atlas in ipairs(textureAtlas.atlases) do
@@ -21,19 +41,20 @@ function textureAtlas.clear()
     end
 end
 
--- TODO - Handle growing the canvas array
--- This just makes new images use themselves instead of adding to atlas
-function textureAtlas.addAtlas()
+-- Set up as layer in canvasArray if possible, otherwise use a new normal canvas
+function textureAtlas.addAtlas(layer)
     local atlasNumber = #textureAtlas.atlases + 1
     local atlasName = string.format("Runtime Atlas #%s", atlasNumber)
     local canvas = textureAtlas.canvasArray
 
     if atlasNumber > textureAtlas.canvases then
-        return
+        canvas = love.graphics.newCanvas(4096, 4096)
+        layer = nil
     end
 
     local atlas = {
         image = canvas,
+        layer = layer,
         width = textureAtlas.width,
         height = textureAtlas.height,
         filename = atlasName,
@@ -75,11 +96,17 @@ function textureAtlas.addImage(atlas, image, filename, layer)
 
             local previousCanvas = love.graphics.getCanvas()
 
-            love.graphics.setCanvas(textureAtlas.canvasArray, layer)
+            if layer <= textureAtlas.canvases then
+                love.graphics.setCanvas(atlas.image, layer)
+
+            else
+                love.graphics.setCanvas(atlas.image)
+            end
+
             love.graphics.draw(image, x, y)
             love.graphics.setCanvas(previousCanvas)
 
-            return true, atlas.image, x, y
+            return true, atlas.image, x, y, atlas.layer
         end
     end
 
@@ -89,15 +116,16 @@ end
 function textureAtlas.addImageFirstAtlas(image, filename, createIfNeeded, onlyCheck)
     for i, atlas in ipairs(textureAtlas.atlases) do
         if not onlyCheck or onlyCheck and onlyCheck == i then
-            local fit, atlasImage, x, y = textureAtlas.addImage(atlas, image, filename, i)
+            local fit, atlasImage, x, y, layer = textureAtlas.addImage(atlas, image, filename, i)
+
             if fit then
-                return atlasImage, x, y, i
+                return atlasImage, x, y, layer
             end
         end
     end
 
     if createIfNeeded ~= false then
-        textureAtlas.addAtlas()
+        textureAtlas.addAtlas(#textureAtlas.atlases + 1)
 
         return textureAtlas.addImageFirstAtlas(image, filename, false, #textureAtlas.atlases)
     end
@@ -113,5 +141,7 @@ function textureAtlas.dumpCanvasImages()
         data:release()
     end
 end
+
+textureAtlas.init()
 
 return textureAtlas
