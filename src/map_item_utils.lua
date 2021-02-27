6598fd0273db97bc21f72aabaabcfdb3ebfdd326
Cruor <cruoratmc@gmail.com>
2021-02-27 21:57:40 +0100

    Map item utils now returns whether items were deleted or not Events are now sent for deleting/adding items

--- src/map_item_utils.lua
+++ src/map_item_utils.lua
@@ -5,6 +5,7 @@
 local utils = require("utils")
 local snapshotUtils = require("snapshot_utils")
 local history = require("history")
+local sceneHandler = require("scene_handler")
 
 local mapItemUtils = {}
 
@@ -12,44 +13,56 @@ function mapItemUtils.deleteRoom(map, room)
     for i, r in ipairs(map.rooms) do
         if r.name == room.name then
             table.remove(map.rooms, i)
+            sceneHandler.sendEvent("editorRoomDeleted", room)
 
-            break
+            return true
         end
     end
+
+    return false
 end
 
 function mapItemUtils.deleteFiller(map, filler)
     for i, f in ipairs(map.fillers) do
         if f.x == filler.x and f.y == filler.y and f.width == filler.width and f.height == filler.height then
             table.remove(map.fillers, i)
+            sceneHandler.sendEvent("editorFillerDeleted", filler)
 
-            break
+            return true
         end
     end
+
+    return false
 end
 
 function mapItemUtils.deleteItem(map, item)
     local itemType = utils.typeof(item)
 
     if itemType == "room" then
-        mapItemUtils.deleteRoom(map, item)
+        return mapItemUtils.deleteRoom(map, item)
 
     elseif itemType == "filler" then
-        mapItemUtils.deleteFiller(map, item)
+        return mapItemUtils.deleteFiller(map, item)
 
     elseif itemType == "table" then
+        local result = false
+
         for subItem, _ in pairs(item) do
-            mapItemUtils.deleteItem(map, subItem)
+            result = result or mapItemUtils.deleteItem(map, subItem)
         end
+
+        return result
     end
 end
 
 function mapItemUtils.addRoom(map, room)
     table.insert(map.rooms, room)
+    sceneHandler.sendEvent("editorRoomAdded", room)
 end
 
 function mapItemUtils.addFiller(map, filler)
     table.insert(map.fillers, filler)
+    sceneHandler.sendEvent("editorFillerAdded", filler)
 end
 
 function mapItemUtils.addItem(map, item)
