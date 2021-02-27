91e41ed1d0abd27038e238759c56ea85819130bc
Cruor <cruoratmc@gmail.com>
2021-02-27 23:50:15 +0100

    Small tool device cleanup No longer needs to proxy keypresses manually Cleaned up room swapping

--- src/input_devices/tool_device.lua
+++ src/input_devices/tool_device.lua
@@ -22,8 +22,7 @@ local toolProxyMt = {
 
 local device = setmetatable({_enabled = true, _type = "device"}, toolProxyMt)
 
--- Don't send clicks that would "swap" the target room
-function device.mouseclicked(x, y, button, istouch, presses)
+local function tryRoomSwap(x, y, button, istouch, presses)
     if state.map ~= nil then
         local currentTool = toolHandler.currentTool
         local actionButton = configs.editor.toolActionButton
@@ -48,11 +47,16 @@ function device.mouseclicked(x, y, button, istouch, presses)
     end
 end
 
-function device.keypressed(key, scancode, isrepeat)
+function device.mouseclicked(x, y, button, istouch, presses)
+    local consume = tryRoomSwap(x, y, button, istouch, presses)
     local currentTool = toolHandler.currentTool
 
-    if currentTool and currentTool.keypressed then
-        currentTool.keypressed(key, scancode, isrepeat)
+    if consume then
+        return true
+    end
+
+    if currentTool and currentTool.mouseclicked then
+        currentTool.mouseclicked(x, y, button, istouch, presses)
     end
 end
 
