88cd9a54f9b8f6b08ddb614e4515dce8a02a28a5
Cruor <cruoratmc@gmail.com>
2021-03-03 22:39:48 +0100

    Strawberries and dream block now uses new node system

--- src/entities/dream_block.lua
+++ src/entities/dream_block.lua
@@ -3,6 +3,7 @@ local dreamBlock = {}
 dreamBlock.name = "dreamBlock"
 dreamBlock.fillColor = {0.0, 0.0, 0.0}
 dreamBlock.borderColor = {1.0, 1.0, 1.0}
+dreamBlock.nodeLineRenderType = "line"
 dreamBlock.placements = {
     name = "dream_block",
     data = {
--- src/entities/strawberry.lua
+++ src/entities/strawberry.lua
@@ -1,16 +1,16 @@
--- TODO - Support nodes
-
 local strawberry = {}
 
 strawberry.name = "strawberry"
 strawberry.depth = -100
+strawberry.nodeLineRenderType = "fan"
 
 function strawberry.texture(room, entity)
     local moon = entity.moon
     local winged = entity.winged
+    local hasNodes = entity.nodes and #entity.nodes > 0
 
     if moon then
-        if winged then
+        if winged or hasNodes then
             return "collectables/moonBerry/ghost00"
 
         else
@@ -19,11 +19,29 @@ function strawberry.texture(room, entity)
 
     else
         if winged then
-            return "collectables/strawberry/wings01"
+            if hasNodes then
+                return "collectables/ghostberry/wings01"
+
+            else
+                return "collectables/strawberry/wings01"
+            end
 
         else
-            return "collectables/strawberry/normal00"
+            if hasNodes then
+                return "collectables/ghostberry/idle00"
+
+            else
+                return "collectables/strawberry/normal00"
+            end
         end
+    end
+end
+
+function strawberry.nodeTexture(room, entity)
+    local hasNodes = entity.nodes and #entity.nodes > 0
+
+    if hasNodes then
+        return "collectables/strawberry/seed00"
     end
 end
 
@@ -50,5 +68,9 @@ strawberry.placements = {
         },
     }
 }
+
+function strawberry.nodeLimits(room, entity)
+    return 0, -1
+end
 
 return strawberry
