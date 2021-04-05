b816e15a264e6364d9329a8bbe10d10f9af00fd5
Cruor <cruoratmc@gmail.com>
2021-04-05 14:43:17 +0200

    Added water entity

---
+++ src/entities/water.lua
@@ -0,0 +1,15 @@
+local water = {}
+
+water.name = "water"
+water.fillColor = {0.0, 0.0, 1.0, 0.4}
+water.borderColor = {0.0, 0.0, 1.0, 1.0}
+water.placements = {
+    name = "water",
+    data = {
+        hasBottom = false
+    }
+}
+
+water.depth = 0
+
+return water
