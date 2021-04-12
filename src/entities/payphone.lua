655779706dfd04fa1fc1a670e76df6330b39fbc3
Cruor <cruoratmc@gmail.com>
2021-04-12 23:56:15 +0200

    Added payphone and pico console entities Moved Wind Trigger into the trigger section in language file

---
+++ src/entities/payphone.lua
@@ -0,0 +1,11 @@
+local payphone = {}
+
+payphone.name = "payphone"
+payphone.depth = 1
+payphone.justification = {0.5, 1.0}
+payphone.texture = "scenery/payphone"
+payphone.placements = {
+    name = "payphone"
+}
+
+return payphone
