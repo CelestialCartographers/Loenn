b3adaf712550c5d8de9a8f787ed1506c06cd390f
Cruor <cruoratmc@gmail.com>
2021-04-23 20:40:21 +0200

    Added white block and cliffside flag entity

---
+++ src/entities/cliffside_flag.lua
@@ -0,0 +1,19 @@
+local cliffsideFlag = {}
+
+cliffsideFlag.name = "cliffside_flag"
+cliffsideFlag.depth = 8999
+cliffsideFlag.justification = {0.0, 0.0}
+cliffsideFlag.placements = {
+    name = "cliffside_flag",
+    data = {
+        index = 0
+    }
+}
+
+function cliffsideFlag.texture(room, entity)
+    local index = entity.index or 0
+
+    return string.format("scenery/cliffside/flag%02d", index)
+end
+
+return cliffsideFlag
---
+++ src/entities/white_block.lua
@@ -0,0 +1,11 @@
+local whiteBlock = {}
+
+whiteBlock.name = "whiteblock"
+whiteBlock.depth = 8990
+whiteBlock.texture = "objects/whiteblock"
+whiteBlock.justification = {0.0, 0.0}
+whiteBlock.placements = {
+    name = "white_block"
+}
+
+return whiteBlock
