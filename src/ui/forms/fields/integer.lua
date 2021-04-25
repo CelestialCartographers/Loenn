81a0555a68683a46ad1a6ac74a2848f188d427b8
Cruor <cruoratmc@gmail.com>
2021-04-25 18:55:31 +0200

    Added value range support to number and integer fields

--- src/ui/forms/fields/integer.lua
+++ src/ui/forms/fields/integer.lua
@@ -8,9 +8,16 @@ integerField.fieldType = "integer"
 function integerField.getElement(name, value, options)
     -- Add extra options and pass it onto string field
 
+    local minimumValue = options.minimumValue or -math.huge
+    local maximumValue = options.maximumValue or math.huge
+
     options.valueTransformer = tonumber
     options.displayTransformer = tostring
-    options.validator = utils.isInteger
+    options.validator = function(v)
+        local number = tonumber(v)
+
+        return utils.isInteger(number) and number >= minimumValue and number <= maximumValue
+    end
 
     return stringField.getElement(name, value, options)
 end
--- src/ui/forms/fields/number.lua
+++ src/ui/forms/fields/number.lua
@@ -7,10 +7,15 @@ numberField.fieldType = "number"
 function numberField.getElement(name, value, options)
     -- Add extra options and pass it onto string field
 
+    local minimumValue = options.minimumValue or -math.huge
+    local maximumValue = options.maximumValue or math.huge
+
     options.valueTransformer = tonumber
     options.displayTransformer = tostring
     options.validator = function(v)
-        return tonumber(v) ~= nil
+        local number = tonumber(v)
+
+        return number ~= nil and number >= minimumValue and number <= maximumValue
     end
 
     return stringField.getElement(name, value, options)
