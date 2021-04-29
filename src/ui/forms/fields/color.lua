2b808daf634a9c825b8838ee533af8603b1f6a16
Cruor <cruoratmc@gmail.com>
2021-04-29 15:49:58 +0200

    Added color picker form field

---
+++ src/ui/forms/fields/color.lua
@@ -0,0 +1,167 @@
+local ui = require("ui")
+local uiElements = require("ui.elements")
+local uiUtils = require("ui.utils")
+local contextMenu = require("ui.context_menu")
+local utils = require("utils")
+local colorPicker = require("ui.widgets.color_picker")
+local configs = require("configs")
+
+local colorField = {}
+
+colorField.fieldType = "color"
+
+colorField._MT = {}
+colorField._MT.__index = {}
+
+local previewOffset = 8
+
+local invalidStyle = {
+    normalBorder = {0.65, 0.2, 0.2, 0.9, 2.0},
+    focusedBorder = {0.9, 0.2, 0.2, 1.0, 2.0}
+}
+
+function colorField._MT.__index:setValue(value)
+    self.field:setText(value)
+    self.currentValue = value
+end
+
+function colorField._MT.__index:getValue()
+    return self.currentValue
+end
+
+function colorField._MT.__index:fieldValid()
+    local parsed, r, g, b = utils.parseHexColor(self:getValue())
+
+    return parsed
+end
+
+local function cacheFieldPreviewColor(element, new)
+    local parsed, r, g, b = utils.parseHexColor(new)
+
+    element._parsed = parsed
+    element._r, element._g, element._b = r, g, b
+
+    return parsed, r, g, b
+end
+
+local function fieldChanged(formField)
+    return function(element, new, old)
+        local parsed, r, g, b = cacheFieldPreviewColor(element, new)
+        local wasValid = formField:fieldValid()
+        local valid = parsed
+
+        formField.currentValue = new
+
+        if wasValid ~= valid then
+            if valid then
+                -- Reset to default
+                formField.field.style = nil
+
+            else
+                formField.field.style = invalidStyle
+            end
+
+            formField.field:repaint()
+        end
+    end
+end
+
+local function getColorPreviewArea(widget)
+    local x, y = widget.screenX, widget.screenY
+    local width, height = widget.width, widget.height
+    local previewSize = height - previewOffset * 2
+    local drawX, drawY = x + width - previewSize - previewOffset, y + previewOffset
+
+    return drawX, drawY, previewSize, previewSize
+end
+
+local function fieldDrawColorPreview(orig, widget)
+    orig(widget)
+
+    local parsed = widget and widget._parsed
+
+    if parsed then
+        local r, g, b = widget._r, widget._g, widget._b
+        local pr, pg, pb, pa = love.graphics.getColor()
+
+        local drawX, drawY, width, height = getColorPreviewArea(widget)
+
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill",  drawX, drawY, width, height)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.rectangle("fill",  drawX + 1, drawY + 1, width - 2, height - 2)
+        love.graphics.setColor(r, g, b)
+        love.graphics.rectangle("fill",  drawX + 2, drawY + 2, width - 4, height - 4)
+        love.graphics.setColor(pr, pg, pb, pa)
+    end
+end
+
+local function shouldShowMenu(widget, x, y, button)
+    local menuButton = configs.editor.contextMenuButton
+    local actionButton = configs.editor.toolActionButton
+
+    if button == menuButton then
+        return true
+
+    elseif button == actionButton then
+        local drawX, drawY, width, height = getColorPreviewArea(widget)
+
+        return utils.aabbCheckInline(x, y, 1, 1, drawX, drawY, width, height)
+    end
+
+    return false
+end
+
+function colorField.getElement(name, value, options)
+    local formField = {}
+
+    local minWidth = options.minWidth or options.width or 160
+    local maxWidth = options.maxWidth or options.width or 160
+
+    local label = uiElements.label(options.displayName or name)
+    local field = uiElements.field(value or "", fieldChanged(formField)):with({
+        minWidth = minWidth,
+        maxWidth = maxWidth
+    }):hook({
+        draw = fieldDrawColorPreview
+    })
+    local fieldWithContext = contextMenu.addContextMenu(
+        field,
+        function()
+            local pickerOptions = {
+                callback = function(data)
+                    field:setText(data.hexColor)
+                end
+            }
+
+            return colorPicker.getColorPicker(field:getText() or "", pickerOptions)
+        end,
+        {
+            shouldShowMenu = shouldShowMenu
+        }
+    )
+
+    cacheFieldPreviewColor(field, value or "")
+    field:setPlaceholder(value)
+
+    if options.tooltipText then
+        label.interactive = 1
+        label.tooltipText = options.tooltipText
+    end
+
+    label.centerVertically = true
+
+    formField.label = label
+    formField.field = field
+    formField.name = name
+    formField.initialValue = value
+    formField.currentValue = value
+    formField.width = 2
+    formField.elements = {
+        label, fieldWithContext
+    }
+
+    return setmetatable(formField, colorField._MT)
+end
+
+return colorField
---
+++ src/ui/widgets/color_picker.lua
@@ -0,0 +1,376 @@
+local ui = require("ui")
+local uiElements = require("ui.elements")
+local uiUtils = require("ui.utils")
+local utils = require("utils")
+local formHelper = require("ui.forms.form")
+local languageRegistry = require("language_registry")
+
+local colorPicker = {}
+
+local pickerAreaMinimumSize = 200
+local sliderMinimumWidth = 40
+local hsvFieldDecimals = 2
+
+local valueRanges = {
+    r = {0, 255},
+    g = {0, 255},
+    b = {0, 255},
+    h = {0, 360},
+    s = {0, 100},
+    v = {0, 100}
+}
+
+local fieldTypes = {
+    r = "integer",
+    g = "integer",
+    b = "integer",
+    h = "number",
+    s = "number",
+    v = "number",
+    hexColor = "hex_color",
+}
+
+local areaHSVPixelCode = [===[
+    uniform float hue;
+
+    vec3 hsv_to_rgb(float h, float s, float v) {
+        return mix(vec3(1.0), clamp((abs(fract(h + vec3(3.0, 2.0, 1.0) / 3.0) * 6.0 - 3.0) - 1.0), 0.0, 1.0), s) * v;
+    }
+
+    vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords)
+    {
+        vec3 rgb = hsv_to_rgb(hue, texture_coords[0], 1 - texture_coords[1]);
+
+        return vec4(rgb[0], rgb[1], rgb[2], 1.0) * color;
+    }
+]===]
+
+local areaHSVShader = love.graphics.newShader(areaHSVPixelCode)
+
+local function updateAreaColors(hue)
+    areaHSVShader:send("hue", hue)
+end
+
+local function getColorPickerArea(h, s, v, width, height)
+    local canvas = love.graphics.newCanvas(width, height)
+
+    updateAreaColors(h)
+
+    return canvas
+end
+
+
+local function getColorPickerSlider(h, s, v, width, height)
+    local canvas = love.graphics.newCanvas(width, height)
+    local imageData = canvas:newImageData()
+
+    imageData:mapPixel(function(x, y, r, g, b, a)
+        local hue = y / (height - 1)
+        local cr, cg, cb = utils.hsvToRgb(hue, 1, 1)
+
+        return cr, cg, cb, 1
+    end, 0, 0, width, height)
+
+    local image = love.graphics.newImage(imageData)
+
+    return image, imageData
+end
+
+local function areaInteraction(interactionData)
+    return function(widget, x, y)
+        local areaSize = interactionData.areaSize
+        local formFields = interactionData.formFields
+
+        local innerX = utils.clamp(x - widget.screenX, 0, areaSize)
+        local innerY = utils.clamp(y - widget.screenY, 0, areaSize)
+
+        local saturation = innerX / areaSize
+        local value = 1 - (innerY / areaSize)
+        local data = formHelper.getFormData(formFields)
+
+        data.s = saturation * 100
+        data.v = value * 100
+
+        formHelper.setFormData(formFields, data)
+    end
+end
+
+local function sliderInteraction(interactionData)
+    return function(widget, x, y)
+        local areaSize = interactionData.areaSize
+        local formFields = interactionData.formFields
+
+        local innerY = utils.clamp(y - widget.screenY, 0, areaSize)
+        local hue = innerY / areaSize
+        local data = formHelper.getFormData(formFields)
+
+        data.h = hue * 360
+
+        updateAreaColors(hue)
+        formHelper.setFormData(formFields, data)
+    end
+end
+
+local function getFormFieldOrder(options)
+    local fieldOrder = {}
+
+    if options.showRGB ~= false then
+        table.insert(fieldOrder, "r")
+        table.insert(fieldOrder, "g")
+        table.insert(fieldOrder, "b")
+    end
+
+    if options.showHSV ~= false then
+        table.insert(fieldOrder, "h")
+        table.insert(fieldOrder, "s")
+        table.insert(fieldOrder, "v")
+    end
+
+    if options.showHex ~= false then
+        table.insert(fieldOrder, "hexColor")
+    end
+
+    return fieldOrder
+end
+
+local function findChangedColorGroup(current, previous)
+    if current.r ~= previous.r or current.g ~= previous.g or current.b ~= previous.b then
+        return "rgb"
+
+    elseif current.h ~= previous.h or current.s ~= previous.s or current.v ~= previous.v then
+        return "hsv"
+
+    elseif current.hexColor ~= previous.hexColor then
+        return "hex"
+    end
+end
+
+-- RGB normalized
+local function updateHsvFields(data, r, g, b)
+    local h, s, v = utils.rgbToHsv(r, g, b)
+
+    data.h = utils.round(h * 360, hsvFieldDecimals)
+    data.s = utils.round(s * 100, hsvFieldDecimals)
+    data.v = utils.round(v * 100, hsvFieldDecimals)
+end
+
+-- HSV Normalized
+local function updateRgbFields(data, h, s, v)
+    local r, g, b = utils.hsvToRgb(h, s, v)
+
+    data.r = utils.round(r * 255)
+    data.g = utils.round(g * 255)
+    data.b = utils.round(b * 255)
+end
+
+-- RGB normalized
+local function updateHexField(data, r, g, b)
+    data.hexColor = utils.rgbToHex(r, g, b)
+end
+
+local function updateFields(data, changedGroup, interactionData)
+    local callback = interactionData.callback
+
+    -- Change group here to make logic simpler
+    if changedGroup == "hex" then
+        local parsed, r, g, b = utils.parseHexColor(data.hexColor)
+
+        updateHsvFields(data, r, g, b)
+
+        changedGroup = "hsv"
+    end
+
+    if changedGroup == "rgb" then
+        local r, g, b = data.r / 255, data.g / 255, data.b / 255
+
+        updateHsvFields(data, r, g, b)
+        updateHexField(data, r, g, b)
+
+    elseif changedGroup == "hsv" then
+        local h, s, v = data.h / 360, data.s / 100, data.v / 100
+        updateRgbFields(data, h, s, v)
+
+        local r, g, b = data.r / 255, data.g / 255, data.b / 255
+        updateHexField(data, r, g, b)
+    end
+
+    updateAreaColors(data.h / 360)
+
+    if callback then
+        callback(data)
+    end
+
+    return data
+end
+
+local function fieldUpdater(interactionData)
+    return function()
+        local formFields = interactionData.formFields
+
+        if formHelper.formValid(formFields) then
+            local formData = formHelper.getFormData(formFields)
+            local changedGroup = findChangedColorGroup(formData, interactionData.previousFormData or formData)
+
+            if changedGroup then
+                local data = updateFields(formData, changedGroup, interactionData)
+
+                formHelper.setFormData(formFields, data)
+            end
+
+            interactionData.previousFormData = formData
+        end
+    end
+end
+
+local function areaDrawing(interactionData)
+    return function(orig, widget)
+        local previousShader = love.graphics.getShader()
+
+        love.graphics.setShader(areaHSVShader)
+        orig(widget)
+        love.graphics.setShader(previousShader)
+
+        local formData = formHelper.getFormData(interactionData.formFields)
+        local areaSize = interactionData.areaSize
+        local x = utils.round((formData.s or 0) / 100 * areaSize)
+        local y = utils.round((1 - (formData.v or 0) / 100) * areaSize)
+        local widgetX, widgetY = widget.screenX, widget.screenY
+        local rightX = widgetX + x
+        local bottomY = widgetY + y
+        local width, height = widget.width, widget.height
+
+        local pr, pg, pb, pa = love.graphics.getColor()
+        local previousLineWidth = love.graphics.getLineWidth()
+
+        love.graphics.setLineWidth(1)
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.rectangle("fill", widgetX, bottomY - 1, width, 3)
+        love.graphics.rectangle("fill", rightX - 1, widgetY, 3, height)
+        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.rectangle("fill", widgetX + 1, bottomY, width - 2, 1)
+        love.graphics.rectangle("fill", rightX, widgetY + 1, 1, height - 2)
+        love.graphics.setLineWidth(previousLineWidth)
+        love.graphics.setColor(pr, pg, pb, pa)
+    end
+end
+
+local function sliderDrawIndication(interactionData)
+    return function(orig, widget)
+        orig(widget)
+
+        local formData = formHelper.getFormData(interactionData.formFields)
+        local areaSize = interactionData.areaSize
+        local sliderWidth = interactionData.sliderWidth
+        local y = utils.round((formData.h or 0) / 360 * areaSize)
+        local widgetX, widgetY = widget.screenX, widget.screenY
+        local sliderY = widgetY + y
+        local width, height = widget.width, widget.height
+        local pr, pg, pb, pa = love.graphics.getColor()
+        local previousLineWidth = love.graphics.getLineWidth()
+
+        love.graphics.setLineWidth(1)
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.rectangle("fill", widgetX, sliderY - 1, sliderWidth, 3)
+        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.rectangle("fill", widgetX + 1, sliderY, sliderWidth - 2, 1)
+        love.graphics.setLineWidth(previousLineWidth)
+        love.graphics.setColor(pr, pg, pb, pa)
+    end
+end
+
+function colorPicker.getColorPicker(hexColor, options)
+    options = options or {}
+
+    local language = languageRegistry.getLanguage()
+    local callback = options.callback or function() end
+
+    local parsed, r, g, b = utils.parseHexColor(hexColor)
+    local h, s, v = utils.rgbToHsv(r or 0, g or 0, b or 0)
+
+    local fieldOrder = getFormFieldOrder(options)
+    local formData = {
+        r = (r or 0) * 255,
+        g = (g or 0) * 255,
+        b = (b or 0) * 255,
+        h = utils.round(h * 360, hsvFieldDecimals),
+        s = utils.round(s * 100, hsvFieldDecimals),
+        v = utils.round(v * 100, hsvFieldDecimals),
+        hexColor = hexColor
+    }
+
+    local formOptions = {
+        columns = 2,
+        fieldOrder = fieldOrder,
+        hideUnordered = true,
+        fields = {}
+    }
+
+    for name, _ in pairs(formData) do
+        local field = formOptions.fields[name] or {}
+        local ranges = valueRanges[name]
+
+        field.fieldType = fieldTypes[name]
+        field.displayName = tostring(language.ui.colorPicker.fieldTypes.name[name])
+        field.tooltipText = tostring(language.ui.colorPicker.fieldTypes.description[name])
+        field.width = 80
+
+        if ranges then
+            field.minimumValue = ranges[1]
+            field.maximumValue = ranges[2]
+        end
+
+        formOptions.fields[name] = field
+    end
+
+    local formBody, formFields = formHelper.getFormBody(formData, formOptions)
+
+    -- Form body height is not properly calculated at this point
+    -- This approximation seems to be accurate enough
+    local areaSize = options.areaSize or math.max(formBody.height * #fieldOrder * 6 / 7, pickerAreaMinimumSize)
+    local sliderWidth = options.sliderWidth or sliderMinimumWidth
+
+    local areaCanvas = getColorPickerArea(h, s, v, areaSize, areaSize)
+    local sliderImage, sliderImageData = getColorPickerSlider(h, s, v, sliderWidth, areaSize)
+
+    local interactionData = {
+        areaCanvas = areaCanvas,
+        sliderImage = sliderImage,
+        sliderImageData = sliderImageData,
+        formFields = formFields,
+        areaSize = areaSize,
+        sliderWidth = sliderWidth,
+        callback = callback
+    }
+
+    local areaElement = uiElements.image(areaCanvas):with({
+        interactive = 1,
+        onDrag = areaInteraction(interactionData),
+        onClick = areaInteraction(interactionData)
+    }):hook({
+        draw = areaDrawing(interactionData)
+    })
+    local sliderElement = uiElements.image(sliderImage):with({
+        interactive = 1,
+        onDrag = sliderInteraction(interactionData),
+        onClick = sliderInteraction(interactionData)
+    }):hook({
+        draw = sliderDrawIndication(interactionData)
+    })
+
+    local columns = {
+        areaElement,
+        sliderElement
+    }
+
+    if #fieldOrder > 0 then
+        table.insert(columns, formBody)
+    end
+
+    local pickerRow = uiElements.row(columns):with({
+        update = fieldUpdater(interactionData)
+    })
+
+    return pickerRow
+end
+
+return colorPicker
