c4dc002b037aca54e64f70a9188f55f16d3e2566
Cruor <cruoratmc@gmail.com>
2021-05-03 19:04:24 +0200

    Added invisible barrier, killbox, lightning and strawberry blockfield entities

---
+++ src/entities/killbox.lua
@@ -0,0 +1,15 @@
+local killbox = {}
+
+killbox.name = "killbox"
+killbox.fillColor = {0.8, 0.4, 0.4, 0.8}
+killbox.borderColor = {0.0, 0.0, 0.0, 0.0}
+killbox.canResize = {true, false}
+killbox.placements = {
+    name = "killbox",
+    data = {
+        width = 8,
+        height = 32
+    }
+}
+
+return killbox
