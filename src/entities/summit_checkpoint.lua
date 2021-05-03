2c4e390ebd32ff2b46856f3303f23679ba7c03c4
Cruor <cruoratmc@gmail.com>
2021-05-03 17:37:36 +0200

    Added summit checkpoint and summit cloud entities

---
+++ src/entities/summit_checkpoint.lua
@@ -0,0 +1,45 @@
+local drawableSprite = require("structs.drawable_sprite")
+
+local summitCheckpoint = {}
+
+summitCheckpoint.name = "summitcheckpoint"
+summitCheckpoint.depth = 8999
+summitCheckpoint.placements = {
+    name = "summit_checkpoint",
+    data = {
+        number = 0
+    }
+}
+
+local backTexture = "scenery/summitcheckpoints/base02"
+local digitBackground = "scenery/summitcheckpoints/numberbg0%d"
+local digitForeground = "scenery/summitcheckpoints/number0%d"
+
+function summitCheckpoint.sprite(room, entity)
+    local number = entity.number or 0
+    local digit1 = math.floor(number % 100 / 10)
+    local digit2 = number % 10
+
+    local backSprite = drawableSprite.spriteFromTexture(backTexture, entity)
+    local backDigit1 = drawableSprite.spriteFromTexture(string.format(digitBackground, digit1), entity)
+    local frontDigit1 = drawableSprite.spriteFromTexture(string.format(digitForeground, digit1), entity)
+    local backDigit2 = drawableSprite.spriteFromTexture(string.format(digitBackground, digit2), entity)
+    local frontDigit2 = drawableSprite.spriteFromTexture(string.format(digitForeground, digit2), entity)
+
+    backDigit1:addPosition(-2, 4)
+    frontDigit1:addPosition(-2, 4)
+    backDigit2:addPosition(2, 4)
+    frontDigit2:addPosition(2, 4)
+
+    local sprites = {
+        backSprite,
+        backDigit1,
+        backDigit2,
+        frontDigit1,
+        frontDigit2
+    }
+
+    return sprites
+end
+
+return summitCheckpoint
