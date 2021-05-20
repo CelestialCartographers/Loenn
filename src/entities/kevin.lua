8d6299da770008a1535a5cfbb0389b8ad219760e
Cruor <cruoratmc@gmail.com>
2021-05-20 22:48:20 +0200

    Added kevin and bounce block entity

---
+++ src/entities/kevin.lua
@@ -0,0 +1,68 @@
+local drawableNinePatch = require("structs.drawable_nine_patch")
+local drawableRectangle = require("structs.drawable_rectangle")
+local drawableSprite = require("structs.drawable_sprite")
+
+local kevin = {}
+
+kevin.name = "crushBlock"
+kevin.depth = 0
+kevin.placements = {}
+
+local axesOptions = {
+    "both", "vertical", "horizontal"
+}
+
+for _, axes in ipairs(axesOptions) do
+    table.insert(kevin.placements, {
+        name = axes,
+        data = {
+            width = 16,
+            height = 16,
+            axes = axes
+        }
+    })
+end
+
+local frameTextures = {
+    none = "objects/crushblock/block00",
+    horizontal = "objects/crushblock/block01",
+    vertical = "objects/crushblock/block02",
+    both = "objects/crushblock/block03"
+}
+
+local ninePatchOptions = {
+    mode = "border",
+    borderMode = "repeat"
+}
+
+local kevinColor = {98 / 255, 34 / 255, 43 / 255}
+local smallFaceTexture = "objects/crushblock/idle_face"
+local giantFaceTexture = "objects/crushblock/giant_block00"
+
+function kevin.sprite(room, entity)
+    local x, y = entity.x or 0, entity.y or 0
+    local width, height = entity.width or 24, entity.height or 24
+
+    local axes = entity.axes or "both"
+    local chillout = entity.chillout
+
+    local giant = height >= 48 and width >= 48 and chillout
+    local faceTexture = giant and giantFaceTexture or smallFaceTexture
+
+    local frameTexture = frameTextures[axes] or frameTextures["both"]
+    local ninePatch = drawableNinePatch.fromTexture(frameTexture, ninePatchOptions, x, y, width, height)
+
+    local rectangle = drawableRectangle.fromRectangle("fill", x + 2, y + 2, width - 4, height - 4, kevinColor)
+    local faceSprite = drawableSprite.spriteFromTexture(faceTexture, entity)
+
+    faceSprite:addPosition(math.floor(width / 2), math.floor(height / 2))
+
+    local sprites = ninePatch:getDrawableSprite()
+
+    table.insert(sprites, 1, rectangle:getDrawableSprite())
+    table.insert(sprites, 2, faceSprite)
+
+    return sprites
+end
+
+return kevin
