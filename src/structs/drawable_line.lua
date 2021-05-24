cddc41ff2b2408a892486613c268768208921af5
Cruor <cruoratmc@gmail.com>
2021-05-24 14:46:56 +0200

    Added offsets to drawable lines Added helper functions to change some attributes after creation

--- src/structs/drawable_line.lua
+++ src/structs/drawable_line.lua
@@ -1,5 +1,6 @@
 local drawableRectangle = require("structs.drawable_rectangle")
 local drawing = require("drawing")
+local utils = require("utils")
 
 local drawableLine = {}
 
@@ -8,9 +9,9 @@ drawableLineMt.__index = {}
 
 local lineExtraWidth = 0.2
 
-local function getRotatedRectangleSprite(x1, y1, x2, y2, color, thickness, smooth)
+local function getRotatedRectangleSprite(x1, y1, x2, y2, color, thickness, smooth, offsetX, offsetY, magnitudeOffset)
     local theta = math.atan2(y2 - y1, x2 - x1)
-    local magnitude = math.sqrt((x1 - x2)^2 + (y1 - y2)^2)
+    local magnitude = math.sqrt((x1 - x2)^2 + (y1 - y2)^2) + magnitudeOffset
 
     local halfThickenss = thickness / 2
     local x = smooth and x1 - lineExtraWidth or x1
@@ -19,7 +20,7 @@ local function getRotatedRectangleSprite(x1, y1, x2, y2, color, thickness, smoot
 
     local sprite = drawableRectangle.fromRectangle("fill", x, y, width, thickness, color):getDrawableSprite()
 
-    sprite:setOffset(0, 0.5 / thickness)
+    sprite:setOffset(offsetX / magnitude, 0.5 / thickness + offsetY / thickness)
     sprite.rotation = theta
 
     return sprite
@@ -29,16 +30,39 @@ function drawableLineMt.__index:getDrawableSprite()
     local points = self.points
     local color = self.color
     local thickness = self.thickness
+    local offsetX, offsetY = self.offsetX, self.offsetY
+    local magnitudeOffset = self.magnitudeOffset
 
     local sprites = {}
 
     for i = 3, #points, 2 do
         local x1, y1, x2, y2 = points[i - 2], points[i - 1], points[i], points[i + 1]
 
-        table.insert(sprites, getRotatedRectangleSprite(x1, y1, x2, y2, color, thickness, true))
+        table.insert(sprites, getRotatedRectangleSprite(x1, y1, x2, y2, color, thickness, true, offsetX, offsetY, magnitudeOffset))
     end
 
     return sprites
+end
+
+function drawableLineMt.__index:setOffset(x, y)
+    self.offsetX = x
+    self.offsetY = y
+end
+
+function drawableLineMt.__index:setMagnitudeOffset(offset)
+    self.magnitudeOffset = offset
+end
+
+function drawableLineMt.__index:setThickness(thickness)
+    self.thickness = thickness
+end
+
+function drawableLineMt.__index:setColor(color)
+    local tableColor = utils.getColor(color)
+
+    if tableColor then
+        self.color = tableColor
+    end
 end
 
 function drawableLineMt.__index:draw()
@@ -62,7 +86,7 @@ function drawableLineMt.__index:draw()
     love.graphics.setLineWidth(previousThickenss)
 end
 
-function drawableLine.fromPoints(points, color, thickness)
+function drawableLine.fromPoints(points, color, thickness, offsetX, offsetY, magnitudeOffset)
     local line = {
         _type = "drawableLine"
     }
@@ -70,6 +94,9 @@ function drawableLine.fromPoints(points, color, thickness)
     line.points = points
     line.color = color
     line.thickness = thickness or 1
+    line.offsetX = offsetX or 0
+    line.offsetY = offsetY or 0
+    line.magnitudeOffset = magnitudeOffset or 0
 
     return setmetatable(line, drawableLineMt)
 end
