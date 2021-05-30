22364701f76a5c3179f06ca893c1bee7021ed8d2
Cruor <cruoratmc@gmail.com>
2021-05-30 15:48:53 +0200

    Added touch switch and gondola entity Added pavement rendering and placement to intro car

--- src/entities/intro_car.lua
+++ src/entities/intro_car.lua
@@ -1,5 +1,6 @@
 local drawableSpriteStruct = require("structs.drawable_sprite")
 local colors = require("xna_colors")
+local utils = require("utils")
 
 local introCar = {}
 
@@ -9,9 +10,16 @@ local pavementTexture = "scenery/car/pavement"
 local wheelsTexture = "scenery/car/wheels"
 
 introCar.name = "introCar"
+introCar.placements = {
+    name = "intro_car",
+    data = {
+        hasRoadAndBarriers = false
+    }
+}
 
 function introCar.sprite(room, entity)
     local sprites = {}
+    local x, y = entity.x, entity.y
     local hasRoadAndBarriers = entity.hasRoadAndBarriers
 
     local bodySprite = drawableSpriteStruct.fromTexture(bodyTexture, entity)
@@ -26,6 +34,22 @@ function introCar.sprite(room, entity)
     table.insert(sprites, wheelSprite)
 
     if hasRoadAndBarriers then
+        utils.setSimpleCoordinateSeed(x, y)
+
+        local pavementWidth = x - 48
+        local columns = math.floor(pavementWidth / 8)
+
+        for column = 0, columns - 1 do
+            local choice = column >= columns - 2 and (column ~= columns - 2 and 3 or 2) or math.random(0, 2)
+            local pavementSprite = drawableSpriteStruct.fromTexture(pavementTexture, entity)
+            pavementSprite:addPosition(column * 8 - x, 0)
+            pavementSprite.depth = -10001
+            pavementSprite:setJustification(0.0, 0.0)
+            pavementSprite:useRelativeQuad(choice * 8, 0, 8, 8)
+
+            table.insert(sprites, pavementSprite)
+        end
+
         local barrier1Sprite = drawableSpriteStruct.fromTexture(barrierTexture, entity)
         barrier1Sprite:addPosition(32, 0)
         barrier1Sprite:setJustification(0.0, 1.0)
@@ -39,11 +63,19 @@ function introCar.sprite(room, entity)
 
         table.insert(sprites, barrier1Sprite)
         table.insert(sprites, barrier2Sprite)
-
-        -- TODO - Add pavement
     end
 
     return sprites
+end
+
+function introCar.selection(room, entity)
+    local bodySprite = drawableSpriteStruct.fromTexture(bodyTexture, entity)
+    local wheelSprite = drawableSpriteStruct.fromTexture(wheelsTexture, entity)
+
+    bodySprite:setJustification(0.5, 1.0)
+    wheelSprite:setJustification(0.5, 1.0)
+
+    return utils.rectangle(utils.coverRectangles({bodySprite:getRectangle(), wheelSprite:getRectangle()}))
 end
 
 return introCar
