da58989579555820d83ebcc96edf07b1c579dca3
Cruor <cruoratmc@gmail.com>
2021-12-06 13:35:07 +0100

    Fixed white rectangle on startup Seems to only happen on Windows? This makes the whole window white instead of a section of it

--- src/conf.lua
+++ src/conf.lua
@@ -3,7 +3,9 @@ function love.conf(t)
 
 	t.window.resizable = true
 	t.window.minwidth = 1280
+	t.window.width = 1280
 	t.window.minheight = 720
+	t.window.height = 720
 
 	t.window.icon = "assets/logo-256.png"
 
