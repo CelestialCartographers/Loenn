95d7dda2e32ede0bb2fd45917ed13c98febd7509
Cruor <cruoratmc@gmail.com>
2021-12-29 11:18:35 +0100

    Fixed render and selection of killboxes Shouldn't depend on any height data, isn't read by the game Removed incorrect border color

--- src/entities/killbox.lua
+++ src/entities/killbox.lua
@@ -1,15 +1,19 @@
+local utils = require("utils")
+
 local killbox = {}
 
 killbox.name = "killbox"
-killbox.fillColor = {0.8, 0.4, 0.4, 0.8}
-killbox.borderColor = {0.0, 0.0, 0.0, 0.0}
+killbox.color = {0.8, 0.4, 0.4, 0.8}
 killbox.canResize = {true, false}
 killbox.placements = {
     name = "killbox",
     data = {
-        width = 8,
-        height = 32
+        width = 8
     }
 }
+
+function killbox.rectangle(room, entity)
+    return utils.rectangle(entity.x, entity.y, entity.width or 8, 32)
+end
 
 return killbox
