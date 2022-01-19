ce28379eedd1549228bab20aa7aa4a01608e9dde
Cruor <cruoratmc@gmail.com>
2022-01-19 23:17:47 +0100

    Added missing entities for fake heart and dream heart

---
+++ src/entities/dream_heart.lua
@@ -0,0 +1,13 @@
+local dreamHeart = {}
+
+dreamHeart.name = "dreamHeartGem"
+dreamHeart.depth = -2000000
+dreamHeart.texture = "collectables/heartGem/0/00"
+dreamHeart.placements = {
+    name = "crystal_heart",
+    data = {
+        color = "Random"
+    }
+}
+
+return dreamHeart
