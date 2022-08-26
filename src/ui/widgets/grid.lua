b6a7f4ca9b3dc90ca099a52ddf01956e28c11e10
Cruor <cruoratmc@gmail.com>
2022-08-26 15:23:26 +0200

    Added function to align multiple grids (below each other) Cleaned up hooks used for "row" alignment for grids

--- src/ui/widgets/grid.lua
+++ src/ui/widgets/grid.lua
@@ -16,14 +16,81 @@ uiElements.add("grid", {
     }
 })
 
+local function columnLayoutChilrenHook(columnElements, columnIndex, style)
+    return function(orig, self)
+        -- Adjust element Y positions to become more "grid like"
+        local offsetY = 0
+        local lowestElement
+        local rows = #columnElements[1]
+        local columnCount = #columnElements
+
+        for y = 1, rows do
+            local rowHeight = 0
+
+            for x = 1, columnCount do
+                local element = columnElements[x][y]
+
+                if element then
+                    element.parent = self
+                    element:layoutLazy()
+
+                    rowHeight = math.max(rowHeight, element.height)
+                end
+            end
+
+            local element = columnElements[columnIndex][y]
+
+            if element then
+                local centerVertically = rawget(element, "centerVertically")
+
+                if centerVertically then
+                    element.y = offsetY
+                    element.y += math.floor((rowHeight - element.height) / 2)
+
+                else
+                    element.y = offsetY
+                end
+            end
+
+            if rowHeight > 0 then
+                offsetY += rowHeight + style.rowSpacing
+            end
+        end
+
+        self.height = offsetY + style.outerPadding * 2 - style.rowSpacing
+        self.innerHeight = offsetY - style.rowSpacing
+
+    end
+end
+
+local function columnWidthHook(grids, columnIndex)
+    return function(orig, self)
+        local width = 0
+
+        for _, grid in ipairs(grids) do
+            local column = grid._columns[columnIndex]
+
+            for _, child in ipairs(column.children) do
+                child:layoutLazy()
+
+                width = math.max(width, child.width)
+            end
+        end
+
+        return width
+    end
+end
+
 function gridElement.getGrid(elements, columnCount)
     local columns = {}
     local columnElements = {}
     local gridStyle = uiElements.grid.__default.style or {}
 
-    local outerPadding = gridStyle.outerPadding or 8
-    local columnSpacing = gridStyle.columnSpacing or gridStyle.spacing or 8
-    local rowSpacing = gridStyle.rowSpacing or gridStyle.spacing or 8
+    local style = {
+        outerPadding = gridStyle.outerPadding or 8,
+        columnSpacing = gridStyle.columnSpacing or gridStyle.spacing or 8,
+        rowSpacing = gridStyle.rowSpacing or gridStyle.spacing or 8
+    }
 
     local column = 1
     local rows = 1
@@ -41,18 +108,6 @@ function gridElement.getGrid(elements, columnCount)
 
         else
             table.insert(targetColumn, element)
-
-            element:hook({
-                layoutLateLazy = function(orig, self)
-                    orig(self)
-
-                    if self.__gridY then
-                        self.y = self.__gridY
-
-                        self.parent:layout()
-                    end
-                end
-            })
         end
 
         column += 1
@@ -69,62 +124,44 @@ function gridElement.getGrid(elements, columnCount)
             style = {
                 spacing = 0
             }
+        }):hook({
+            layoutChildren = columnLayoutChilrenHook(columnElements, i, style)
         })
     end
 
     local row = uiElements.row(columns):with({
         style = {
-            padding = outerPadding,
-            spacing = columnSpacing,
+            padding = style.outerPadding,
+            spacing = style.columnSpacing,
         }
     })
 
-    row:hook({
-        layout = function(orig, self)
-            orig(self)
-
-            -- Adjust element Y positions to become more "grid like"
-            local offsetY = 0
-            local lowestElement
-
-            for y = 1, rows + 1 do
-                local rowHeight = 0
-
-                for x = 1, columnCount do
-                    local element = columnElements[x][y]
-
-                    if element then
-                        rowHeight = math.max(rowHeight, element.height)
-                    end
-                end
-
-                for x = 1, columnCount do
-                    local element = columnElements[x][y]
-
-                    if element then
-                        local centerVertically = rawget(element, "centerVertically")
-
-                        if centerVertically then
-                            element.__gridY = offsetY
-                            element.__gridY += math.floor((rowHeight - element.height) / 2)
-
-                        else
-                            element.__gridY = offsetY
-                        end
-                    end
-                end
-
-                if rowHeight > 0 then
-                    offsetY += rowHeight + rowSpacing
-                end
-            end
-
-            self.height = offsetY + outerPadding * 2 - rowSpacing
-            self.innerHeight = offsetY - rowSpacing
-        end
-    })
+    row._columns = columns
 
     return row
+end
+
+function gridElement.alignColumns(grids)
+    -- Don't need to do anything
+    if not grids or #grids < 2 then
+        return
+    end
+
+    local columnCount = #grids[1]._columns
+
+    for _, grid in ipairs(grids) do
+        if #grid._columns ~= columnCount then
+            return false, "Column counts mismatching"
+        end
+    end
+
+    for _, grid in ipairs(grids) do
+        for columnIndex, column in ipairs(grid._columns) do
+            column:hook({
+                calcWidth = columnWidthHook(grids, columnIndex)
+            })
+        end
+    end
 end
 
 return gridElement
