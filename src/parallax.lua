1c652e7d02fbb9291cacef8284402f3bc8c2ba0d
Cruor <cruoratmc@gmail.com>
2022-09-01 17:35:21 +0200

    Added field options for blendmode Added texture dropdown options, not currently hooked up due to being very slow

--- src/parallax.lua
+++ src/parallax.lua
@@ -1,4 +1,11 @@
+local atlases = require("atlases")
+local utils = require("utils")
+local mods = require("mods")
+
 local parallax = {}
+
+local parallaxTexturePrefix = "bgs/"
+local parallaxTexturePath = "Graphics/Atlases/Gameplay/bgs"
 
 local fieldOrder = {
     "texture", "only", "exclude", "tag",
@@ -44,11 +51,64 @@ local defaultData = {
     tag = ""
 }
 
+-- Texture options are intended to be added on demand
 local fieldInformation = {
     color = {
         fieldType = "color"
+    },
+    blendmode = {
+        options = {
+            "additive",
+            "alphablend"
+        },
+        editable = false
+    },
+    texture = {
+        options = {},
+        editable = true
     }
 }
+
+function parallax.getParallaxNames()
+    local res = {}
+    local added = {}
+
+    -- Any loaded sprites
+    for name, sprite in pairs(atlases.gameplay) do
+        if utils.startsWith(name, parallaxTexturePrefix) then
+            added[name] = true
+            added[sprite.meta.filename] = true
+
+            table.insert(res, name)
+        end
+    end
+
+    -- Mod content sprites
+    -- Some of these might have already been loaded
+    local filenames = mods.findModFiletype(parallaxTexturePath, "png")
+    local parallaxPathLength = #parallaxTexturePath
+
+    for i, name in ipairs(filenames) do
+        if not added[name] then
+            local nameNoExt, ext = utils.splitExtension(name)
+            if ext == "png" then
+                -- Remove mod specific path, keep bgs/ prefix
+                local firstSlashIndex = utils.findCharacter(nameNoExt, "/")
+                local resourceName = nameNoExt:sub(firstSlashIndex + parallaxPathLength - 2)
+
+                if not added[resourceName] then
+                    table.insert(res, resourceName)
+                end
+            end
+
+            if yield and i % 100 == 0 then
+                coroutine.yield()
+            end
+        end
+    end
+
+    return res
+end
 
 function parallax.defaultData(style)
     return defaultData
