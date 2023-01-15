dafa79ee98a9dd75783d2b7224825612fb84148c
Cruor <cruoratmc@gmail.com>
2023-01-15 20:58:41 +0100

    Fixed rendering of vanilla intro crushers Fixed default flags for intro crusher, accidentaly got a space after the , Fixed selection box for intro crusher node

--- src/entities/intro_crusher.lua
+++ src/entities/intro_crusher.lua
@@ -1,4 +1,5 @@
 local fakeTilesHelper = require("helpers.fake_tiles")
+local utils = require("utils")
 
 local introCrusher = {}
 
@@ -6,17 +7,22 @@ introCrusher.name = "introCrusher"
 introCrusher.depth = 0
 introCrusher.nodeLineRenderType = "line"
 introCrusher.nodeLimits = {1, 1}
+introCrusher.fieldInformation = fakeTilesHelper.getFieldInformation("tiletype")
 introCrusher.placements = {
     name = "intro_crusher",
     data = {
         tiletype = "3",
-        flags = "1, 0b",
+        flags = "1,0b",
         width = 8,
         height = 8
     }
 }
 
 introCrusher.sprite = fakeTilesHelper.getEntitySpriteFunction("tiletype", false)
-introCrusher.fieldInformation = fakeTilesHelper.getFieldInformation("tiletype")
+introCrusher.nodeSprite = fakeTilesHelper.getEntitySpriteFunction("tiletype", false)
+
+function introCrusher.nodeRectangle(room, entity, node)
+    return utils.rectangle(node.x or 0, node.y or 0, entity.width or 8, entity.height or 8)
+end
 
 return introCrusher
