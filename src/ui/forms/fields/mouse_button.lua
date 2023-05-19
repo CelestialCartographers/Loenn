f7aabfe9f4e14d44ace5a07136e4fbec6176d6bc
Cruor <cruoratmc@gmail.com>
2023-05-19 14:37:14 +0200

    Added simple hotkey/mouse button fields to forms Will be used for the settings window

---
+++ src/ui/forms/fields/mouse_button.lua
@@ -0,0 +1,85 @@
+local ui = require("ui")
+local uiElements = require("ui.elements")
+local uiUtils = require("ui.utils")
+
+local stringField = require("ui.forms.fields.string")
+local iconUtils = require("ui.utils.icons")
+local utils = require("utils")
+local languageRegistry = require("language_registry")
+
+local hotkeyStruct = require("structs.hotkey")
+local inputCapturingDevice = require("input_devices.input_capture_device")
+
+local mouseField = {}
+
+mouseField._MT = {}
+mouseField._MT.__index = {}
+
+function mouseField._MT.__index:setValue(value)
+    self.field:setText(tostring(value))
+    self.currentValue = value
+end
+
+function mouseField._MT.__index:getValue()
+    return self.currentValue
+end
+
+function mouseField._MT.__index:fieldValid()
+    return true
+end
+
+mouseField.fieldType = "mouse_button"
+
+local function captureMouseButton(formField, buttonElement)
+    local previousText = buttonElement.text
+    local language = languageRegistry.getLanguage()
+
+    buttonElement:setText(tostring(language.ui.userInput.capturing))
+
+    return function(button)
+        if button then
+            formField:setValue(button)
+            formField:notifyFieldChanged()
+            buttonElement:setText(tostring(button))
+
+        else
+            buttonElement:setText(previousText)
+        end
+    end
+end
+
+function mouseField.getElement(name, value, options)
+    local formField = {}
+
+    local language = languageRegistry.getLanguage()
+    local buttonText = value and tostring(value) or tostring(language.ui.userInput.noValue)
+
+    local label = uiElements.label(options.displayName or name)
+    local buttonElement = uiElements.button(buttonText, function(self, x, y, button)
+        inputCapturingDevice.captureMouseButton(captureMouseButton(formField, self))
+    end)
+
+    local minWidth = options.minWidth or options.width or 160
+    local maxWidth = options.maxWidth or options.width or 160
+
+    buttonElement:with({
+        minWidth = minWidth,
+        maxWidth = maxWidth
+    })
+
+    label.centerVertically = true
+
+    formField.label = label
+    formField.field = buttonElement
+    formField.name = name
+    formField.initialValue = value
+    formField.currentValue = value
+    formField.width = 2
+    formField.elements = {
+        label, buttonElement
+    }
+
+    return setmetatable(formField, mouseField._MT)
+end
+
+return mouseField
