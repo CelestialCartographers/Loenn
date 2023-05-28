70d972a5b78a3a37579978570d91debcc2ffdf4d
Cruor <cruoratmc@gmail.com>
2023-05-28 14:27:11 +0200

    Added rotation and flipping to spikes Made it easier to create mutliple handlers The data for this is already in the options for the rotation/flipping logic
    Added deprecation warning for the old createEntityHandler arguments, with so many new options its not flexible to keep around
    When that is deprecated the other functions (getPlacements etc) can also get their arguments cleaned up
    
    Spikes have a more options in general now:
    placementData: Default data from placement
    fieldInformation: Default field information for placement
    variantKey: Which entity attribute will have the spike texture, by default it is "type". This value can also be a table, in which case the first value is used for sprites.
    placementName: The format string used for the placement name, useful in cases where you only have one placement or want a different format
    Variant key and placementData tested with Core Mode Spike from Maddie Helping Hand and seems to do the job

--- src/entities/trigger_spikes_original.lua
+++ src/entities/trigger_spikes_original.lua
@@ -1,13 +1,13 @@
 local spikeHelper = require("helpers.spikes")
 
-local spikeUp = spikeHelper.createEntityHandler("triggerSpikesOriginalUp", "up", false, true)
-local spikeDown = spikeHelper.createEntityHandler("triggerSpikesOriginalDown", "down", false, true)
-local spikeLeft = spikeHelper.createEntityHandler("triggerSpikesOriginalLeft", "left", false, true)
-local spikeRight = spikeHelper.createEntityHandler("triggerSpikesOriginalRight", "right", false, true)
-
-return {
-    spikeUp,
-    spikeDown,
-    spikeLeft,
-    spikeRight
+local spikeOptions = {
+    directionNames = {
+        up = "triggerSpikesOriginalUp",
+        down = "triggerSpikesOriginalDown",
+        left = "triggerSpikesOriginalLeft",
+        right = "triggerSpikesOriginalRight"
+    },
+    originalTriggerSpike = true
 }
+
+return spikeHelper.createEntityHandlers(spikeOptions)
