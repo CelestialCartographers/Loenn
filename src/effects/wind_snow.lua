a780300da89e7dd27015fec8f87a318c6a8adabe
Cruor <cruoratmc@gmail.com>
2023-08-18 15:30:16 +0200

    Added missing color attribute to wind snow effect

--- src/effects/wind_snow.lua
+++ src/effects/wind_snow.lua
@@ -1,5 +1,14 @@
 local windSnow = {}
 
 windSnow.name = "windSnow"
+windSnow.fieldInformation = {
+    color = {
+        fieldType = "color",
+        allowEmpty = true
+    }
+}
+windSnow.defaultData = {
+    color = ""
+}
 
 return windSnow
