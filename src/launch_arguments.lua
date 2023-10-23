3d3f2089e6769f87413e9941e4747cb725ac7a51
Cruor <cruoratmc@gmail.com>
2023-10-23 17:05:22 +0200

    Fixed mistake in launch_arguments.lua
    Not quite sure how this happened...

--- src/launch_arguments.lua
+++ src/launch_arguments.lua
@@ -30,7 +30,6 @@ function launchArguments.updateArguments(raw)
     launchArguments.rawArguments = raw
     launchArguments.parsed = parser:parse(arguments)
 
-    return launchArguments._parsed
     return launchArguments.parsed
 end
 
