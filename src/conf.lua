f7f2a6017fe96d2e66e3df31a43830fd79597024
Cruor <cruoratmc@gmail.com>
2024-03-24 10:23:41 +0100

    Enabled the joystick module again
    Changed conf.lua indentation from tabs to spaces

--- src/conf.lua
+++ src/conf.lua
@@ -10,79 +10,78 @@ local configs = require("configs")
 local persistence = require("persistence")
 
 local function getWindowSize()
-	local width, height = 1280, 720
+    local width, height = 1280, 720
 
-	if configs.general.persistWindowSize then
-		width = persistence.windowWidth or width
-		height = persistence.windowHeight or height
-	end
+    if configs.general.persistWindowSize then
+        width = persistence.windowWidth or width
+        height = persistence.windowHeight or height
+    end
 
-	return width, height
+    return width, height
 end
 
 local function getWindowFullscreen()
-	local fullscreen, fullscreenType = false, "desktop"
+    local fullscreen, fullscreenType = false, "desktop"
 
-	if configs.general.persistWindowFullscreen then
-		fullscreen = persistence.windowFullscreen or fullscreen
-		fullscreenType = persistence.windowFullscreenType or fullscreenType
-	end
+    if configs.general.persistWindowFullscreen then
+        fullscreen = persistence.windowFullscreen or fullscreen
+        fullscreenType = persistence.windowFullscreenType or fullscreenType
+    end
 
-	return fullscreen, fullscreenType
+    return fullscreen, fullscreenType
 end
 
 local function getWindowPosition()
-	local x, y
+    local x, y
 
-	if configs.general.persistWindowPosition then
-		x, y = persistence.windowX, persistence.windowY
-	end
+    if configs.general.persistWindowPosition then
+        x, y = persistence.windowX, persistence.windowY
+    end
 
-	return x, y
+    return x, y
 end
 
 local function getWindowDisplay()
-	local index = 1
+    local index = 1
 
-	if configs.general.persistWindowPosition then
-		index = persistence.windowDisplay
-	end
+    if configs.general.persistWindowPosition then
+        index = persistence.windowDisplay
+    end
 
-	return index
+    return index
 end
 
 function love.conf(t)
-	local width, height = getWindowSize()
-	local fullscreen, fullscreenType = getWindowFullscreen()
-	local windowX, windowY = getWindowPosition()
-	local windowDisplay = getWindowDisplay()
+    local width, height = getWindowSize()
+    local fullscreen, fullscreenType = getWindowFullscreen()
+    local windowX, windowY = getWindowPosition()
+    local windowDisplay = getWindowDisplay()
 
-	t.console = configs.debug.displayConsole
-	t.version = "11.0"
+    t.console = configs.debug.displayConsole
+    t.version = "11.0"
 
-	t.window.resizable = true
+    t.window.resizable = true
 
-	t.window.minwidth = math.min(1080, width)
-	t.window.width = width
-	t.window.minheight = math.min(720, height)
-	t.window.height = height
+    t.window.minwidth = math.min(1080, width)
+    t.window.width = width
+    t.window.minheight = math.min(720, height)
+    t.window.height = height
 
-	t.window.fullscreen = fullscreen
-	t.window.fullscreentype = fullscreenType
+    t.window.fullscreen = fullscreen
+    t.window.fullscreentype = fullscreenType
 
-	t.window.x = windowX
-	t.window.y = windowY
-	t.window.display = windowDisplay
+    t.window.x = windowX
+    t.window.y = windowY
+    t.window.display = windowDisplay
 
-	t.window.title = meta.title
-	t.window.icon = "assets/logo-256.png"
+    t.window.title = meta.title
+    t.window.icon = "assets/logo-256.png"
 
-	t.window.vsync = configs.graphics.vsync
+    t.window.vsync = configs.graphics.vsync
 
-	-- Not used
-	t.modules.audio = false
-	t.modules.joystick = false
-	t.modules.physics = false
-	t.modules.sound = false
-	t.modules.video = false
+    -- Not used
+    t.modules.audio = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.video = false
 end
