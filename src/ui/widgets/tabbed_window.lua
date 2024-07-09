60468e35c891c9a457d2067f62df6ee2c0d533a5
Cruor <cruoratmc@gmail.com>
2024-07-09 18:40:26 +0200

    Fixed scroll in tabbed windows
    Requires a workaround with columns in columns
    This is a good enough solution

--- src/ui/widgets/tabbed_window.lua
+++ src/ui/widgets/tabbed_window.lua
@@ -2,16 +2,12 @@ local ui = require("ui")
 local uiElements = require("ui.elements")
 local uiUtils = require("ui.utils")
 
-local widgetUtils = require("ui.widgets.utils")
-
 local tabbedWindow = {}
 
 function tabbedWindow.createWindow(title, tabs, initialTab)
     initialTab = initialTab or 1
 
-    local tabContent = uiElements.group({
-        tabs[initialTab].content
-    })
+    local tabContent = tabs[initialTab].content
     local windowTabItems = {}
 
     for i, tab in ipairs(tabs) do
@@ -23,10 +19,30 @@ function tabbedWindow.createWindow(title, tabs, initialTab)
     end
 
     local function tabsCallback(list, target)
+        local parent = list.parent
         local newChild = windowTabItems[target].content
 
-        tabContent:removeChild(tabContent.children[1])
-        tabContent:addChild(newChild)
+        while #parent.children > 1 do
+            parent:removeChild(parent.children[#parent.children])
+        end
+
+        -- Check if new child is column, automatically use its children if it is
+        -- Add all elements if table, otherwise add the single element
+
+        local childType = newChild.__type
+
+        if childType == "column" then
+            newChild = newChild.children
+        end
+
+        if #newChild > 0 then
+            for _, nc in ipairs(newChild) do
+                list.parent:addChild(nc)
+            end
+
+        else
+            list.parent:addChild(newChild)
+        end
     end
 
     local tabsElement = uiElements.listH(windowTabItems, tabsCallback):with(
@@ -42,7 +58,8 @@ function tabbedWindow.createWindow(title, tabs, initialTab)
         tabContent
     }):with({
         minWidth = 128
-    })
+    }):with(uiUtils.fillHeight(true))
+
     local window = uiElements.window(title, windowContent)
 
     return window, windowContent
