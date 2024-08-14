26cd1572e1d724722b4bfaf5a462d67f36ca003e
Cruor <cruoratmc@gmail.com>
2024-08-14 12:42:14 +0200

    Pass Celeste atlas files through runtime atlas without adding to canvas
    No reason to add them to the canvas, just uses more vram when the image itself is plenty to use directly

--- src/runtime_atlas.lua
+++ src/runtime_atlas.lua
@@ -179,6 +179,18 @@ function textureAtlas.removeImage(image, quad, layer)
 end
 
 function textureAtlas.addImageFirstAtlas(image, filename, createIfNeeded, onlyCheck)
+    if not image then
+        return nil, 0, 0, -1
+    end
+
+    local imageWidth, imageHeight = image:getDimensions()
+
+    -- Doesn't need a canvas, already has the right size
+    -- This mostly applies to Celeste atlas files
+    if imageWidth == textureAtlas.width and imageHeight == textureAtlas.height then
+        return image, 0, 0
+    end
+
     for i, atlas in ipairs(textureAtlas.atlases) do
         if not onlyCheck or onlyCheck and onlyCheck == i then
             local fit, atlasImage, x, y, layer = textureAtlas.addImage(atlas, image, filename, i)
