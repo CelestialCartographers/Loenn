12d1329e76fcd263c985246bc89febc27609c1c9
Cruor <cruoratmc@gmail.com>
2025-04-25 20:22:32 +0200

    Added Jellyfish Barrier as alternative name to Seeker Barrier

--- src/entities/seeker_barrier.lua
+++ src/entities/seeker_barrier.lua
@@ -5,6 +5,7 @@ seekerBarrier.depth = 0
 seekerBarrier.color = {0.25, 0.25, 0.25, 0.8}
 seekerBarrier.placements = {
     name = "seeker_barrier",
+    alternativeName = "jellyfish_barrier",
     data = {
         width = 8,
         height = 8
