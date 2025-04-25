a9dccc99ddafcf6c19b54cc3cbd8d45a446ea09c
Cruor <cruoratmc@gmail.com>
2025-04-25 18:26:49 +0200

    Added positionMode to music fade trigger
    Added by Everest, more convenient than the vanilla direction attribute

--- src/triggers/music_fade.lua
+++ src/triggers/music_fade.lua
@@ -5,15 +5,20 @@ local musicFade = {}
 musicFade.name = "musicFadeTrigger"
 musicFade.category = "audio"
 musicFade.fieldInformation = {
+    -- Legacy attribute, superceded by positionMode added by Everest
     direction = {
         options = enums.music_fade_trigger_directions,
+        editable = false
+    },
+    positionMode = {
+        options = enums.trigger_position_modes,
         editable = false
     }
 }
 musicFade.placements = {
     name = "music_fade",
     data = {
-        direction = "leftToRight",
+        positionMode = "NoEffect",
         fadeA = 0.0,
         fadeB = 1.0,
         parameter = ""
