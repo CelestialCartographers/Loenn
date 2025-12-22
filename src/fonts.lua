258c5ea29f3ce0b47835448248e67b7aa5287e16
Lucky_boy <3079351998@qq.com>
2025-12-22 22:15:52 +0800

    update hi-res font image

--- src/fonts.lua
+++ src/fonts.lua
@@ -5,7 +5,7 @@ local font = {}
 
 
 if (HI_RES_FONT) then
-    font.fontString = [=[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]" ]=]
+    font.fontString = [=[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/():;%&`'*#=[]"_{}<>^~ ]=]
     font.font = love.graphics.newImageFont("fonts/hi-res_pixel_font.png", font.fontString, 1)
     -- 以 pico8 的 scale 为 基准
     font.fontScale = 0.5
