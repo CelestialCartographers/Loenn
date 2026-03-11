fd9a6d0238dfd9b97e08f954a91964f71e1a477e
Cruor <cruoratmc@gmail.com>
2026-03-11 22:00:21 +0100

    Fix scenery tile crash after refactor

--- src/celeste_render.lua
+++ src/celeste_render.lua
@@ -446,7 +446,7 @@ function celesteRender.getTilesBatch(room, tiles, meta, scenery, fg, randomMatri
     for x = 1, width do
         for y = 1, height do
             local tile = tilesMatrix:getInbounds(x, y)
-            local sceneryTile = sceneryMatrix and sceneryMatrix:getInbounds(x, y, -1)
+            local sceneryTile = sceneryMatrix and sceneryMatrix:getInbounds(x, y, -1) or -1
 
             if sceneryTile > -1 then
                 local quad = celesteRender.getOrCacheScenerySpriteQuad(sceneryTile)
